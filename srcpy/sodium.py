8b0260fc1ef6248c54ce15cd1f74045d9f8b164d
Satin Wuker <74630829+SatinWuker@users.noreply.github.com>
2021-11-23 23:51:03 -0800

    Fixed the bugs of can't use if stmts in function

--- srcpy/sodium.py
+++ srcpy/sodium.py
@@ -12,7 +12,6 @@ DIGITS = {
 # Keywords
 KW_PRINT   = 'print'
 KW_VAR     = 'store'
-KW_EXECUTE = 'execute'
 KW_DEF     = 'define'
 KW_IF      = 'if'
 KW_WHEN    = 'when'
@@ -22,7 +21,6 @@ KW_END     = 'end'
 INSTRUCTIONS = {
     KW_PRINT,
     KW_VAR,
-    KW_EXECUTE,
     KW_DEF,
     KW_IF,
     KW_WHEN,
@@ -31,12 +29,15 @@ INSTRUCTIONS = {
 
 # Operators
 OPERATORS = {
-    'is',
-    'isnt',
+    '+',
+    '-',
+    '*',
+    '/',
     '<',
-    '>'
+    '>',
+    'is',
+    'isnt'
 }
-
 
 # Tokens
 TT_INSTRUCTION = 'INSTRUCTION'
@@ -44,10 +45,9 @@ TT_OPERATORS   = 'OPERATORS'
 TT_STRING      = 'STRING'
 TT_NUM         = 'NUMBER'
 TT_IDENTIFYER  = 'IDENTIFYER'
-TT_SEPARATOR   = 'SEPARATOR'
 
 variables = {}
-functions = {}
+instructions = {}
 
 def typeof(string=''):
     if string == '': return None
@@ -85,7 +85,7 @@ def evaluate(tokens, types, temp_var={}):
         if tokens[i] == '(':
             ops.append(tokens[i])
 
-        elif tokens[i].isdigit():
+        elif types[i] == TT_NUM:
             values.append(int(tokens[i]))
 
         elif types[i] == TT_IDENTIFYER:
@@ -95,9 +95,8 @@ def evaluate(tokens, types, temp_var={}):
                 var_value = str(temp_var[tokens[i]])
             values.append(int(var_value) if var_value.isdigit() else var_value)
 
-        elif tokens[i][0] == '"' and tokens[i][-1] == '"':
+        elif types[i] == TT_STRING:
             values.append(tokens[i][1: -1])
-
 
         elif tokens[i] == ')':
             while len(ops) != 0 and ops[-1] != '(':
@@ -118,7 +117,6 @@ def evaluate(tokens, types, temp_var={}):
 
             ops.append(tokens[i])
 
-
     while len(ops) != 0:
         val2 = values.pop()
         val1 = values.pop()
@@ -126,14 +124,6 @@ def evaluate(tokens, types, temp_var={}):
         values.append(applyOp(val1, val2, op))
 
     return values[-1]
-
-class Instruction:
-    def __init__(self, name='', connectors=[]):
-        self.name = name
-        self.connectors = connectors
-
-    def __repr__(self):
-        return f'{self.name}'
 
 
 ##################################################################################################
@@ -171,8 +161,6 @@ def parse(tokens):
             types.append(TT_INSTRUCTION)
         elif i in OPERATORS:
             types.append(TT_OPERATORS)
-        elif i in SEPAEATORS:
-            types.append(TT_SEPARATOR)
         elif typeof(i) == 'string':
             types.append(TT_STRING)
         elif typeof(i) == 'number':
@@ -184,7 +172,7 @@ def parse(tokens):
 
 
 class Instruction:
-    def __init__(self, name, connectors=[], stmts=[]):
+    def __init__(self, name, connectors, stmts):
         self.name = name
         self.connectors = connectors
         self.stmts = stmts
@@ -193,45 +181,54 @@ class Instruction:
         return f'{self.name}'
 
 class Interpreter:
-    def __init__(self):
-        self.current_cl = 0
-        self.executing_cl = 0
-
+    def __init_func__(self):
         self.in_function = False
         self.func_name = ""
         self.stmts_in_func = []
         self.connectors = []
+        self.temp_vars = {}
 
+    def __init__(self):
+        self.current_cl = 0
+        self.executing_cl = 0
+        self.__init_func__()
 
-    def PRINT(self, val):
-        if '\\n' in val:
-            for i in val.split("\\n")[:-1]:
+    def PRINT(self, expr):
+        if '\\n' in expr:
+            for i in expr.split("\\n")[:-1]:
                 stdout.write(f'{i}\n')
             return
-        stdout.write(f'{val}')
+        stdout.write(f'{expr}')
 
     def DEFINE(self, name, connectors):
         self.current_cl += 1
         self.in_function = True
         self.func_name = name
         self.connectors = connectors
-        functions.update({self.func_name : None})
+        instructions.update({self.func_name : None})
 
-    def interpret(self, tokens, types, temp_var={}):
+    def IF(self, cond):
+        self.current_cl += 1
+        if cond == 'true':
+            self.executing_cl += 1
+
+    def interpret(self, tokens, types, arguments=[], connectors=[]):
         if not types or types[0] != TT_IDENTIFYER and types[0] != TT_INSTRUCTION: return
 
         if tokens[0] == KW_END:
-            if self.executing_cl == self.current_cl: self.executing_cl -= 1
-            self.current_cl -= 1
+            if self.executing_cl == self.current_cl:
+                self.executing_cl -= 1
 
-            functions[self.func_name] = Instruction(self.func_name, self.connectors, self.stmts_in_func)
-            self.in_function = False
-            self.func_name = ''
-            self.connectors = []
+                if self.in_function:    # End a function
+                    instructions[self.func_name] = Instruction(self.func_name, self.connectors, self.stmts_in_func)
+                    self.__init_func__()
+
+            self.current_cl -= 1
             return
 
         if self.in_function:
             self.stmts_in_func.append([tokens, types])
+            return
 
         if self.current_cl != self.executing_cl:
             return
@@ -239,27 +236,26 @@ class Interpreter:
         # When calling a function
         if types[0] == TT_IDENTIFYER:
             "INSTRUCTION ARGS"
-            instruc = functions[tokens[0]]
+            instruc = instructions[tokens[0]]
             ARGS = tokens[1:]
-            tp_var = {}
             for i in range(len(ARGS)):
-                tp_var.update({instruc.connectors[i]:ARGS[i]})
+                self.temp_vars.update({instruc.connectors[i]:ARGS[i]})
 
             for i in instruc.stmts:
-                self.interpret(i[0], i[1], tp_var)
+                self.interpret(i[0], i[1], arguments=ARGS, connectors=instruc.connectors)
             return
 
         if tokens[0] == KW_PRINT:
             "print EXPR"
-            EXPR = str(evaluate(tokens[1:], types[1:], temp_var))
-            self.PRINT(EXPR)
+            self.PRINT(expr=str(evaluate(tokens[1:], types[1:], self.temp_vars)))
             return
 
         if tokens[0] == KW_VAR:
             "store EXPR VAR"
-            EXPR = tokens[1:len(tokens) - 1]
             VAR = tokens[-1]
-            variables.update({VAR: evaluate(EXPR, types[1:len(tokens) - 1], temp_var)})
+            if VAR in connectors:
+                VAR = arguments[connectors.index(tokens[-1])]
+            variables.update({VAR: evaluate(tokens[1:len(tokens) - 1], types[1:len(tokens) - 1], self.temp_vars)})
             return
 
         if tokens[0] == KW_DEF:
@@ -268,28 +264,20 @@ class Interpreter:
             return
 
         if tokens[0] == KW_IF:
-            "if CONDI"
-            CONDI = evaluate(tokens[1:], types[1:], temp_var)
-            self.current_cl += 1
-            if CONDI == 'true':
-                self.executing_cl += 1
-
+            "if COND"
+            self.IF(cond=evaluate(tokens[1:], types[1:], self.temp_vars))
 
 def main():
     current_line = 0
-    text = read_file(file_name=argv[-1])
-
     interp = Interpreter()
 
-    for stmt in text:
+    for stmt in read_file(file_name=argv[-1]):
         current_line += 1
         tokens = tokenize(stmt)
         try:
             interp.interpret(tokens, parse(tokens))
         except Exception as e:
             stdout.write(f"\nException in line {current_line}: {e}\n")
-
-
 
 if __name__ == '__main__':
     main()
